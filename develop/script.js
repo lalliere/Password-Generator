f65218462a2ad55b12fa4f982ba3941c2702a4b6
lalliere <58668197+lalliere@users.noreply.github.com>
2020-01-18 09:53:43 -0500

    Completed Javascript

--- develop/script.js
+++ develop/script.js
@@ -1,12 +1,37 @@
-// Assignment Code
 let generateBtn = document.querySelector("#generate");
 
-//User prompts
-let length = parseInt(prompt("How many characters would you like your password to contain? Please choose a number between 8 and 128."));
-let special = confirm("Click OK to confirm using special characters in your password.");
-let numbers = confirm("Click OK to confirm using numbers in your password.");
-let lowercase = confirm("Click OK to confirm using lowercase letters in your password.");
-let uppercase = confirm("Click OK to confirm using uppercase letters in your password.");
+function writePassword() {
+    prompts();
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+    // copyBtn.removeAttribute("disabled");
+    // copyBtn.focus();
+}
+
+let length = 0;
+let special = false;
+let numbers = false;
+let lowercase = false;
+let uppercase = false;
+
+function prompts() {
+    checkLength();
+    special = confirm("Click OK to confirm using special characters in your password.");
+    numbers = confirm("Click OK to confirm using numbers in your password.");
+    lowercase = confirm("Click OK to confirm using lowercase letters in your password.");
+    uppercase = confirm("Click OK to confirm using uppercase letters in your password.");
+}
+
+function checkLength() {
+    length = parseInt(prompt("How many characters would you like your password to contain? Please choose a number between 8 and 128."));
+    if ((length < 8) || (length > 128)) {
+        alert("Please input a valid length for your password.");
+        checkLength();
+    }
+}
 
 //blank array 
 let pass = [];
@@ -17,73 +42,42 @@ let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let smallLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-//validate length of password; if outside of the parameters, alert the user
-function checkLength () {
-  if ((length < 8) || (length > 128)) {
-    alert("Please input a valid length for your password.");
-  }
+
+//if user confirms they want special characters, add the characters array to the blank "pass" array
+function push() {
+    if (special) {
+        pass.push(characters);
+    }
+
+    //if user confirms they want numbers, add the digits array to the blank pass array
+    if (numbers) {
+        pass.push(digits);
+    }
+
+    //if user confirms they want lowercase letters, add the smallLetters array to the blank pass array
+    if (lowercase) {
+        pass.push(smallLetters);
+    }
+
+    //if user confirms they want uppercase letters, add the bigLetters array to the blank pass array
+    if (uppercase) {
+        pass.push(bigLetters);
+    }
 }
 
-checkLength()
-
-//if user confirms they want special characters, add the characters array to the blank pass array
-if (special) {
-  pass.push(characters);
+function generatePassword() {
+    let passString = "";
+    for (i = 0; i < length; i++) {
+        push();
+        let randomChoices = pass[Math.floor(Math.random() * pass.length)];
+        let selection = randomChoices[Math.floor(Math.random() * randomChoices.length)];
+        
+        passString += selection;
+    }
+    return passString;
 }
 
-//if user confirms they want numbers, add the digits array to the blank pass array
-if (numbers) {
-  pass.push(digits);
-}
-
-//if user confirms they want lowercase letters, add the smallLetters array to the blank pass array
-if (lowercase) {
-  pass.push(smallLetters);
-}
-
-//if user confirms they want uppercase letters, add the bigLetters array to the blank pass array
-if (uppercase) {
-  pass.push(bigLetters);
-}
-
-
-//console.log(pass)
-
-//randomly choose a value from the pass array; loop the same amount of times as the user designated length
-
-
-//I need a function that takes the output of randomChoices and stores it in a string
-function createPass(passOpts, passLength) {
-  let passString = "";
-  
-  for (let i = 0; i < passLength; i++) {
-    let randomChoices = passOpts[Math.floor(Math.random() * pass.length)][Math.floor(Math.random() * pass.length)]; 
-    passString += randomChoices; 
-  }
-  
-  return passString;
-
-}
-
-let randPass = createPass(pass, length);
-
-
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
-}
-
-function copyToClipboard() {
-  // BONUS 
-}
-
-// Add event listener to generate button
+// //  Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
